8f2cb2fa5d89eb227528df538024b5c6f61c1c97
Petr Adam <adam@solarsoft.cz>
2025-03-19 17:20:15 +0100

    Introduced prettier

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import { QueryClient } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
@@ -19,12 +19,12 @@ const persister = createSyncStoragePersister({
 function App() {
   return (
     <PersistQueryClientProvider
-    client={queryClient}
-    persistOptions={{ persister }}
-  >
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <CurrencyConverter />
     </PersistQueryClientProvider>
   );
 }
 
-export default App
+export default App;
--- app/src/CurrencyConverter.tsx
+++ app/src/CurrencyConverter.tsx
@@ -3,32 +3,38 @@ import { parseExchangeRates } from './helpers/parseExchangeRates';
 import { useQuery } from '@tanstack/react-query';
 
 export function CurrencyConverter() {
+  function useExchangeRates() {
+    return useQuery({
+      queryKey: ['exchangeRates'],
+      queryFn: async (): Promise<Array<CurrencyExchangeRate>> => {
+        const response = await fetch(
+          'https://api.allorigins.win/raw?url=https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt'
+        );
 
-    function useExchangeRates() {
-        return useQuery({
-          queryKey: ['exchangeRates'],
-          queryFn: async (): Promise<Array<CurrencyExchangeRate>> => {
-            const response = await fetch('https://api.allorigins.win/raw?url=https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt');
-      
-            return parseExchangeRates(await response.text());
-          },
-        })
-      }
-    
-    const { status, data, error, isFetching } = useExchangeRates();
+        return parseExchangeRates(await response.text());
+      },
+    });
+  }
 
-    if (isFetching) return <p>Loading...</p>;
-    if (error) return <p>Something went wrong!</p>;
+  const { status, data, error, isFetching } = useExchangeRates();
 
-    return   <>{data && <ul>
-        {data.map(exchangeRate => (
-          <li key={exchangeRate.code}>{exchangeRate.currency}</li>
-        ))}
-      </ul>}  </>;    
-  };
+  if (isFetching) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong!</p>;
 
+  return (
+    <>
+      {data && (
+        <ul>
+          {data.map((exchangeRate) => (
+            <li key={exchangeRate.code}>{exchangeRate.currency}</li>
+          ))}
+        </ul>
+      )}{' '}
+    </>
+  );
+}
 
-// TODO good ol fetch way  
+// TODO good ol fetch way
 // const fetchExchangeRates = async () => {
 //   const response = await fetch('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt');
 //   if (!response.ok) {
