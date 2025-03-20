f091cca3551ac06dfbe790127cfb69b9825c39d1
Petr Adam <adam@solarsoft.cz>
2025-03-20 12:50:15 +0100

    CurrencyConverter parseFloat

--- app/src/CurrencyConverter.tsx
+++ app/src/CurrencyConverter.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { CurrencyExchangeRate } from "./model/currency";
-import { convertCurrency } from "./helpers/convertCurrency";
+import { useState } from 'react';
+import { CurrencyExchangeRate } from './model/currency';
+import { convertCurrency } from './helpers/convertCurrency';
 
 interface Props {
   exchangeRate: CurrencyExchangeRate;
@@ -12,21 +12,19 @@ const CurrencyConverter = ({ exchangeRate }: Props) => {
   return (
     <div>
       <h2>
-        {exchangeRate.amount} {exchangeRate.currency} ({exchangeRate.code}):{" "}
-        {exchangeRate.rate}
+        {exchangeRate.amount} {exchangeRate.currency} ({exchangeRate.code}): {exchangeRate.rate}
       </h2>
       <input
         type="number"
         value={amount}
-        // todo use floating points instead of integers
-        onChange={(e) => setAmount(parseInt(e.target.value, 10))}
-        placeholder="Amount"
+        onChange={(e) => setAmount(parseFloat(e.target.value))}
+        placeholder="Amount (CZK)"
+        step="0.01"
       />
       <div>
         {amount > 0 && (
           <p>
-            {amount} {exchangeRate.currency} =
-            <span> {convertCurrency(amount, exchangeRate).toFixed(3)} CZK</span>
+            {amount} {exchangeRate.currency} = <span> {convertCurrency(amount, exchangeRate).toFixed(2)} CZK</span>
           </p>
         )}
       </div>
